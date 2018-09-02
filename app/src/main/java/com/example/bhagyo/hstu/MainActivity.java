96c2710fadf01fd5461a53c7c711db2d1d651a09
bhagyo <prachinmon1995@gmail.com>
2018-09-03 02:35:34 +0600

    general commit

---
+++ app/src/main/java/com/example/bhagyo/hstu/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.bhagyo.hstu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webvw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webvw=findViewById(R.id.d_webvw);
+
+        WebSettings webSettings = webvw.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webvw.setWebViewClient(new WebViewClient());
+        webvw.loadUrl("http://www.hstu.ac.bd/");
+    }
+    public void onBackPressed(){
+        if(webvw.canGoBack()){
+            webvw.goBack();
+        }
+        else
+            super.onBackPressed();
+    }
+}
